8b1b8f8ec5f6315b26ddb0af9718f38aa8bd0866
Chizunum Nlerum <Chizunum@gmail.com>
2025-09-06 08:52:11 +0100

    Admin

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
 from django.contrib.auth import login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
-from django.views.generic import DetailView
 from django.views.generic.detail import DetailView
 from .models import Library, Book
+
 
 
 # Function-based view to list all books (renders template)
@@ -20,28 +20,57 @@ class LibraryDetailView(DetailView):
     context_object_name = "library"
 
 # --- Authentication Views ---
+# Register view
 def register(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request, user)  # auto login after register
-            return redirect("list_books")
+            login(request, user)  # log in new user
+            return redirect("home")  # redirect to home page
     else:
         form = UserCreationForm()
-    return render(request, "relationship_app/register.html", {"form": form})
+    return render(request, "register.html", {"form": form})
 
+# Login view
 def login_view(request):
     if request.method == "POST":
-        form = AuthenticationForm(data=request.POST)
+        form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-            return redirect("list_books")
+            return redirect("home")
     else:
         form = AuthenticationForm()
-    return render(request, "relationship_app/login.html", {"form": form})
+    return render(request, "login.html", {"form": form})
 
+# Logout view
 def logout_view(request):
     logout(request)
-    return render(request, "relationship_app/logout.html")
+    return redirect("home")
+
+
+    # Check functions
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+# Admin view
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, "relationship_app/admin_view.html")
+
+# Librarian view
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, "relationship_app/librarian_view.html")
+
+# Member view
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, "relationship_app/member_view.html")
