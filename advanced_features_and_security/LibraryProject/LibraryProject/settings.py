0ba636f280e2e1b539bb1f5fb85a0f84c09fd70c
Chizunum Nlerum <Chizunum@gmail.com>
2025-09-14 11:59:23 +0100

    Implementing a Custom User Model in Django

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -122,8 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-AUTH_USER_MODEL = "relationship_app.CustomUser"   # ✅ capital C, exact class name
+AUTH_USER_MODEL = "bookshelf.CustomUser"
 
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
 
 # Register your models here.
@@ -8,3 +10,40 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ("title", "author", "publication_year")  # shows these columns in list view
     list_filter = ("publication_year", "author")            # filters on the right sidebar
     search_fields = ("title", "author")                     # search box
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ("username", "email", "date_of_birth", "is_staff", "is_active")
+    list_filter = ("is_staff", "is_active", "date_of_birth")
+
+    fieldsets = (
+        (None, {"fields": ("username", "email", "password")}),
+        ("Personal Info", {"fields": ("date_of_birth", "profile_photo")}),
+        ("Permissions", {"fields": ("is_staff", "is_active", "is_superuser", "groups", "user_permissions")}),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "username",
+                    "email",
+                    "date_of_birth",
+                    "profile_photo",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_active",
+                ),
+            },
+        ),
+    )
+
+    search_fields = ("username", "email")
+    ordering = ("username",)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
